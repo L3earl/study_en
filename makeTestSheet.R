ae08392c87c7f216b82a53a9fb56079a0ce072cd
Earl Lee <sainknight87@gmail.com>
2017-02-27 19:37:56 +0900

    1차 버전 완성 전, 사무실은 추우니 집에서 마저 작업하자

--- makeTestSheet.R
+++ makeTestSheet.R
@@ -1,121 +1,44 @@
 ### 전처리 된 단어들의 뜻과 예문을 네이버 사전에서 스크래핑하여, data.frame 형태로 엑셀로 저장
-## 같은 단어인지 확인함 (acase sensitive)
 
-## 품사를 확인함
+## 전처리 된 단어를 불러옴
+temp.text <- paste0('import("', output.folder, '/words.frequency_', filename.url, '.xlsx")')
+data.words <- eval(parse(text=temp.text))
 
+## 불러온 단어를 1번부터 온라인 사전에 넣고, 뜻을 긁어와 list (vectors) 로 만든 후, 합쳐서 데이터 프레임으로 만듬
+data.words.1 <- as.data.frame(data.words[,1], stringsAsFactors = FALSE)
 
-## 해당 단어의 URL에서 상세 URL을 찾아내서 접속
 meaning.data <- function(x){
-  main.url <- paste0('http://endic.naver.com/search.nhn?sLn=kr&searchOption=all&query=' , x)
-  main.html <- read_html(main.url)
+  url <- paste0('http://dic.daum.net/search.do?q=' , x, '&dic=eng&search_first=Y')
+  temp.html <- read_html(url) 
   
-  temp <- html_nodes(main.html, "body .word_num .list_e2 .first span a")[1]
+  error.1 <- html_nodes(temp.html, ".tit_speller")
+  if(length(error.1) != 0){
+    return(NA)
+  }
   
-  temp2 <- strapplyc(as.character(temp), paste0('(/.*query=', x, ')'), simplify = TRUE)
-  words.url <- paste0('http://endic.naver.com', temp2)
-  words.html <- read_html(words.url)
-  temp3 <- html_nodes(words.html, '.list_a3 .meanClass .align_line')
-  temp4 <- strapplyc(as.character(temp3), paste0('(<span class="fnt_k.*</span>)\r'), simplify = TRUE)
-  temp5 <- gsub('\\[',',', temp4)
-  result <- gsub('[^가-힣(),]', '', temp5)
+  temp.txt <- html_nodes(temp.html, ".search_box .cleanword_type .txt_search")
   
+  if(length(temp.txt) == 0){
+    return(NA)
+  }
+  
+  meanings <- html_text(temp.txt)
+  
+  result <- append(x, meanings, after = 2)
   return(result)
 }
 
-meaning.data('function')
+# 함수 실행
+temp.list <- apply(as.data.frame(data.words.1[,1]), 1, meaning.data)  
 
-test <- apply(as.data.frame(data.words[,1]), 1, meaning.data)
+# 함수 결과인 리스트를 합쳐서 데이터 프레임으로 만듬
+foo <- 6
+temp.dtfm <- t(sapply(temp.list, '[', 1:foo))
+colnames(temp.dtfm) <- c(1:ncol(temp.dtfm))
+  
+words.meaning <- temp.dtfm[(!(is.na(temp.dtfm[,1])) & grepl('[^a-zA-Z]', temp.dtfm[,2])), ]
+  
+temp.text <- paste0('export(words.meaning, "', output.folder, '/testSheet_', filename.url, '.xlsx")')
+eval(parse(text=temp.text))
 
-
-
-test <- 'function'
-tc<- 'piece'
-
-url <- paste0('http://endic.naver.com/search.nhn?sLn=kr&searchOption=all&query=' , tc)
-test <- read_html(url) 
-
-test2 <- html_nodes(test, "body .word_num .list_e2 .first span a")[1]
-head(test2)
-test2
-
-t3 <- strapplyc(as.character(test2), paste0('(/.*query=', tc, ')'), simplify = TRUE)
-url2 <- paste0('http://endic.naver.com', t3)
-t4 <- read_html(url2)
-t5 <- html_nodes(t4, '.list_a3 .meanClass .align_line')
-t5
-
-
-ddt <- html_text(t5)
-kkng <- gsub('\\t ', '/', ddt)
-kkn2 <- gsub('[[:cntrl:]]', '', kkng)
-qq<- gsub('[" ]', '', kkn2)
-qq2 <- gsub('/', ' ', qq)
-qq3 <- gsub('예문닫기', '', qq2)
-qq4 <- gsub('^ {1}', '', qq3)
-qq5 <- gsub(' ${1}', '', qq4)
-
-#############
-td<- 'method'
-
-url <- paste0('http://dic.daum.net/search.do?q=' , td, '&dic=eng&search_first=Y')
-test <- read_html(url) 
-
-test2 <- html_nodes(test, ".search_box .cleanword_type .txt_search")
-head(test2)
-test2
-
-html_text(test2)
-
-t3 <- strapplyc(as.character(test2), paste0('(/.*query=', tc, ')'), simplify = TRUE)
-url2 <- paste0('http://endic.naver.com', t3)
-t4 <- read_html(url2)
-t5 <- html_nodes(t4, '.list_a3 .meanClass .align_line')
-t5
-
-
-ddt <- html_text(t5)
-kkng <- gsub('\\t ', '/', ddt)
-kkn2 <- gsub('[[:cntrl:]]', '', kkng)
-qq<- gsub('[" ]', '', kkn2)
-qq2 <- gsub('/', ' ', qq)
-qq3 <- gsub('예문닫기', '', qq2)
-qq4 <- gsub('^ {1}', '', qq3)
-qq5 <- gsub(' ${1}', '', qq4)
-
-############
-
-ddt2 <- gsub('[[:cntrl:]]', '', ddt)
-
-
-
-t6 <- strapplyc(as.character(t5), paste0('(<span class="fnt_k.*</span>)\r'), simplify = TRUE)
-t6
-
-t7 <- gsub('\\[',',', t6)
-t7
-
-t8 <- gsub('[^가-힣(),]', '', t7)
-t8
-
-#내가 수집하길 원하는 페이지 주소 
-url <- "http://endic.naver.com/search.nhn?sLn=kr&query=game&searchOption=entry_idiom" 
-mainPage <- read_html(url) #해당 url 페이지의 html tag를 가져와서 parsing함.
-
-#parsing한 데이터 에서 css가 '' 인 것을 찾아라.
-pageInfo <- html_nodes(mainPage, "body p .fnt_k05")
-
-head(pageInfo)
-str(pageInfo)
-
-pageInfo %>% html_text()
-
-
-
-#rm(list = ls(all = TRUE))
-gc()
-
-rnorm
-?rnorm
-help.search('rnorm')
-args('rnorm')
-help.search('NLP')
+##
--- preprocessing.R
+++ preprocessing.R
@@ -1,71 +1,86 @@
-test <- raw.data.list %>% lapply(readLines) %>% lapply(as.data.table)
-str(test)
-tt2 <- unlist(test, use.names = FALSE)
-str(tt2)
-tt2[1]
-tt3 <- tolower(tt2)
-str(tt3)
-sum(nchar(tt3[c(4200:4300)]))
-sum(nchar(tt3[c(4800:4900)]))
-sum(nchar(tt3[c(4900:5000)]))
+### 데이터 전처리 과정
 
-test <- raw.data.list %>% lapply(readLines) %>% lapply(as.data.table) 
-sum(nchar(unlist(test)))
-test <- raw.data.list %>% lapply(readLines) %>% lapply(as.data.table) %>% rbindlist() 
-sum(nchar(unlist(test)))
-test <- raw.data.list %>% lapply(readLines) %>% lapply(as.data.table) %>% rbindlist() %>% tolower()
-sum(nchar(test))
+## 웹의 내용을 불러오는 경우
+# 해당 url의 텍스트를 가져와 영문, 띄어쓰기를 빼고 삭제
+url.2 <- url.1 %>% read_html() %>% html_text() %>% tolower()
+url.3 <- gsub('[^a-z ]', ' ', url.2)
+raw.data.words <- gsub('\\', ' ', url.3, fixed = TRUE)
+##
 
-tt4 <- gsub('[^a-z ]', '', tt3)
-str(tt4)
 
-## Some text.
-system.time({
-s <- tt4
+## PC 내에 있는 데이터를 불러와 사용하는 경우, 소문자 변환, 띄어쓰기와 영문을 뺀 나머지 삭제 (- 같은 것을 삭제하였기 때문에, 몇몇 단어들은 뜻을 정확히 전달 못 할 수 있음)
+if(FALSE){
+raw.data.list <- dir(list.dirs(), pattern = 'en.txt', full.names = TRUE)
 
-s <- as.String(s)
+temp.data <- raw.data.list %>% lapply(readLines) %>% lapply(as.data.table) %>% 
+            unlist(use.names =FALSE) %>%  tolower()
 
-## Need sentence and word token annotations.
+temp.data2 <- gsub('[^a-z ]', '', temp.data)
+raw.data.words <- gsub('\\', '', temp.data2, fixed = TRUE)
+}
+##
+
+
+## 데이터를 word 단위로 자르고, 품사 Tag를 붙힘. openNLP 패키지의 example 사용
+s <- as.String(raw.data.words)
+
+# Need sentence and word token annotations.
 sent_token_annotator <- Maxent_Sent_Token_Annotator()
 word_token_annotator <- Maxent_Word_Token_Annotator()
 a2 <- annotate(s, list(sent_token_annotator, word_token_annotator))
 
 pos_tag_annotator <- Maxent_POS_Tag_Annotator()
-pos_tag_annotator
 a3 <- annotate(s, pos_tag_annotator, a2)
-a3
-## Variant with POS tag probabilities as (additional) features.
-head(annotate(s, Maxent_POS_Tag_Annotator(probs = TRUE), a2))
 
-## Determine the distribution of POS tags for word tokens.
+# Determine the distribution of POS tags for word tokens.
 a3w <- subset(a3, type == "word")
 tags <- sapply(a3w$features, `[[`, "POS")
-tags
-table(tags)
-## Extract token/POS pairs (all of them): easy.
+
+# 뭔지 모르는 내용, 나중에 힌트 얻을 때 실행시켜보면 좋을 듯 하다
+if(FALSE){
+# Extract token/POS pairs (all of them): easy.
 test3 <- sprintf("%s/%s", s[a3w], tags)
 
-## Extract pairs of word tokens and POS tags for second sentence:
+# Extract pairs of word tokens and POS tags for second sentence:
 a3ws2 <- annotations_in_spans(subset(a3, type == "word"),
                               subset(a3, type == "sentence")[2L])[[1L]]
-sprintf("%s/%s", s[a3ws2], sapply(a3ws2$features, `[[`, "POS"))
-})
+a3ws3 <- sprintf("%s/%s", s[a3ws2], sapply(a3ws2$features, `[[`, "POS"))
+}
+##
 
-head(a3w)
-test2 <- subset(a3, features == 'POS')
-head(test2)
-str(test2)
-test2
-?sapply
-a3$features
 
-test4 <- strsplit(test3,"/")
-
-str(test3)
-
+## 어떤 품사로 구분된 단어를 쓸지 확인
+if(FALSE){
+gw <- 'NN'
+t5 <- s[a3w[which(tags == gw)]] 
+unique(t5)
 table(tags)
-t5 <- s[a3w[which(tags == c('JJ'))]]
-tdw <- unique(s[a3w])
-str(tdw)
-head(tdw)
-table(tdw)
+}
+##
+
+
+## 단어장에 필요한 태그가 붙은 단어만 따로 뽑아내고, 3글자 이상의 단어만 뽑아내어, 빈도순으로 정렬, data.frame 형식
+tag.words <- s[a3w[which(tags == 'IN' | tags == 'JJ' | tags == 'JJR' | tags == 'JJS'
+                         | tags == 'NN' | tags == 'NNP' | tags == 'NNPS' | tags == 'NNS'
+                         | tags == 'RB' | tags == 'VB' | tags == 'VBP' | tags == 'VBZ')]]
+
+temp.tw <- as.data.frame(tag.words[nchar(tag.words) >= 3])
+colnames(temp.tw) <- 'words'
+temp.tw$num <- 1
+
+temp.tw2 <- aggregate(num ~ words, temp.tw, sum)
+words.frequency <- arrange(temp.tw2, desc(num))
+##
+
+
+## 
+temp.text <- paste0('export(words.frequency, "', output.folder, '/words.frequency_', filename.url, '.xlsx")')
+eval(parse(text=temp.text))
+
+
+## 추가로 할 수 있는 전처리
+# s, es, os 로 끝나는 단어 찾아서 해당 문자 제거 후, 같은 단어가 있는지 확인, 있다면 해당 단어들에서 s들 삭제
+# ves -> f/fe, ies -> y, es -> rm, s -> rm
+
+
+
